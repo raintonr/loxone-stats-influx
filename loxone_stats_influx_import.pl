cbf6f794d80f23c500a18d32a93535668ed16219
Robin Rainton <robin@rainton.com>
2020-05-14 14:08:13 +0200

    Added command line options.

--- loxone_stats_influx_import.pl
+++ loxone_stats_influx_import.pl
@@ -20,11 +20,17 @@
 #	},
 #	
 #	"uuids" : {
-#		"1234abcd-037d-9763-ffffffee1234abcd": {"measurement": "temperature", "tags": {"room": "Kitchen"} },
-#		"1234abcd-005f-8965-ffffffee1234abcd": {"measurement": "humidity", "tags": {"room": "Kitchen"} },
+#		"1234abcd-037d-9763-ffffffee1234abcd": {"measurement": "temperature", "tags": {"room": "Kitchen"}, "active_dt": "2020-04-01T12:00:00" },
+#		"1234abcd-005f-8965-ffffffee1234abcd": {"measurement": "humidity", "tags": {"room": "Kitchen"}, "inactive_dt": "2020-04-01T12:00:00"  },
 #		"1234abcd-0052-0f08-ffffffee1234abcd": {"measurement": "AnythingYouLike", "tags": {"lots": "OfTags", "AsMany": "AsYouLike"} }
 #	}
 #}
+#
+# The variables active_dt & inactive_dt are used to start/end import of
+# that uuid on given dates (in timezone of file being processed). This
+# is handy if the Loxone object was deleted and the value re-created
+# as a different type. The start/end are necessary to avoid duplicate
+# values.
 #
 # This code automatically adds the tags, 'uuid' and 'src' to all values.
 #
@@ -37,18 +43,24 @@ use File::Basename;
 use File::Find;
 use File::Slurp;
 use File::Touch;
+use Getopt::Long;
 use JSON;
 use HTTP::Tiny;
 use POSIX;
 use Time::Local;
 
-# More logging?
-# TODO: should be command line option
-use constant VERBOSE => 1;
+# Command line options
+my $verbose = 0;
+my $dry_run = 0;
+my $ignore_stamp = 0;
+my $config_file = $ENV{HOME} . '/.loxone_stats_influx';
+my @stat_files;
 
-# Dry-run?
-# TODO: should be command line option
-use constant DRY_RUN => 0;
+GetOptions("verbose"  => \$verbose,
+           "dry" => \$dry_run,
+           "ignorestamp" => \$ignore_stamp,
+           "config=s" => \$config_file,
+           "files=s{1,}" => \@stat_files);
 
 # Byte positions in Loxone stat files
 use constant TITLE_START => 12;
@@ -61,8 +73,8 @@ use constant READING_LENGTH => 8;
 use constant TIMESTAMP_OFFSET => 1230768000;
 
 # Load config. JSON is easy to use in Node.js too.
-# TODO: should be command line option
-my $confref = from_json(read_file($ENV{HOME} . '/.loxone_stats_influx'));
+printf("Loading config from: %s\n", $config_file) if ($verbose);
+my $confref = from_json(read_file($config_file));
 
 # TODO: we really should allow use of ENV variables (particularly $HOME)
 # in the config file at some point. 
@@ -70,10 +82,13 @@ my $url = sprintf("%s://%s:%s/write?db=%s&precision=s", $confref->{'influxdb'}->
 
 # Find out when we last ran (this file will be updated with the date
 # of the newest file processed on exit).
+my $lastmtime = 0;
 my $stamp_file = $ENV{HOME} . '/.loxone_stats_influx_import.stamp';
-my $lastmtime = (stat($stamp_file))[9];
-$lastmtime = 0 if (!defined($lastmtime));
-printf("Last mtime: %d\n", $lastmtime) if (VERBOSE);
+if (!$ignore_stamp) {
+        $lastmtime = (stat($stamp_file))[9];
+        $lastmtime = 0 if (!defined($lastmtime));
+}
+printf("Last mtime: %d\n", $lastmtime) if ($verbose);
 
 sub uuid {
 	return (split/\./, basename shift)[0];
@@ -97,7 +112,7 @@ sub process_file {
 	my $uuid = uuid($stat_file);
 	my $tags = get_tags($uuid);
 	if (!defined($tags)) {
-		printf("No tags found for %s. Skipping.", $uuid) if (VERBOSE);
+		printf("No tags found for %s. Skipping.", $uuid) if ($verbose);
 	} else {
 		printf("Processing %s ... ", $stat_file);
 
@@ -109,12 +124,12 @@ sub process_file {
 		# TODO: this is pretty horrible, is there a better way?
 		my $title = unpack("Z64", substr($bin, TITLE_START));
 
-                printf(" %s (readings: %d)\n", $title, $readings) if(VERBOSE);
+                printf(" %s (readings: %d)\n", $title, $readings) if($verbose);
                 my $data_length = (int((DATE_LENGTH + $readings * READING_LENGTH) / READING_BOUNDARY) + 1) * READING_BOUNDARY;
                 
                 # Work out the next data boundary
                 my $data_position = (int((TITLE_START + length($title)) / READING_BOUNDARY) + 1) * READING_BOUNDARY;
-                printf("Data starting at %d with length %d\n", $data_position, $data_length) if(VERBOSE);
+                printf("Data starting at %d with length %d\n", $data_position, $data_length) if($verbose);
 
 		my $http = HTTP::Tiny->new;
 		my $points = 0;
@@ -139,12 +154,12 @@ sub process_file {
 			$points++;
 		}
 
-		if (DRY_RUN) {
-                        print Dumper($form_data) if (VERBOSE);
+		if ($dry_run) {
+                        print Dumper($form_data) if ($verbose);
                         printf("%d points read.\n", $points);
                 } else {
                         my $response = $http->request('POST', $url, { content => $form_data });
-                        print Dumper($response) if (VERBOSE);
+                        print Dumper($response) if ($verbose);
                         printf("%d points read & posted.\n", $points);
 
                         my $mtime = (stat($stat_file))[9];
@@ -154,11 +169,9 @@ sub process_file {
 	}
 }
 
-my @stat_files;
-
 sub filter_files {
 	my $name = $File::Find::name;
-	print $name, "\n" if (VERBOSE);
+	print $name, "\n" if ($verbose);
 
 	# Only files matching our pattern.
 	# Files are something like:
@@ -166,14 +179,17 @@ sub filter_files {
 	if ($name =~ /.*\/(([0-9a-f]{8})-([0-9a-f]{4})-([0-9a-f]{4})-([0-9a-f]{16}))\.([0-9]{6})/) {
 		# Only files new since we last fully ran.
 		my $mtime = (stat($name))[9];
-		printf("mtime: %d\n", $mtime);
+		printf("mtime for %s: %d\n", $name, $mtime) if ($verbose);
 		if ($mtime > $lastmtime) {
 			push @stat_files, $name;
 		}
 	}
 }
 
-find({wanted => \&filter_files, no_chdir => 1}, ($confref->{'loxone'}->{'statsdir'}));
+# Don't look for files ourself if given on the command line
+if (!scalar(@stat_files)) {
+        find({wanted => \&filter_files, no_chdir => 1}, ($confref->{'loxone'}->{'statsdir'}));
+}
 printf("Found %d files to process\n", scalar(@stat_files));
 
 my $file_count = 0;
@@ -189,7 +205,7 @@ foreach my $file (sort @stat_files) {
 # This isn't imperative because duplicate runs just overwrite data,
 # they don't duplicate it.
 if ($file_count && $lastmtime > 0) {
-	printf("Setting mtime: %d\n", $lastmtime) if (VERBOSE);
+	printf("Setting mtime: %d\n", $lastmtime) if ($verbose);
 	my $touch = File::Touch->new(mtime => $lastmtime, mtime_only => 1);
 	$touch->touch($stamp_file);
 }
